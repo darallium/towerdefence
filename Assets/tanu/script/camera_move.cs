755b319b6fae8bbb7cae0fc4361a0abf9cd6bda8
tanu <115785841+shogo53421@users.noreply.github.com>
2023-10-26 20:31:27 +0900

    カメラの追従 (#2)
    Co-authored-by: cat53421 <cat53421@outlook.jp>

---
+++ Assets/tanu/script/camera_move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject targetObject;
+
+	// starting value for the Lerp
+	static float t = 0.02f;
+
+	void Update()
+	{
+		// animate the position of the game object...
+		Vector2 camera_position = this.transform.position;
+		Vector2 player_position = targetObject.transform.position;
+		transform.position = new Vector3(Mathf.Lerp(camera_position.x, player_position.x, t), Mathf.Lerp(camera_position.y, player_position.y, t), -1);
+	}
+}
